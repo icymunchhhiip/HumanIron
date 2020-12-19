1f20367a6a9fc419142ca8c41b5afe1331aa551d
icymunchhhiip <fsec1sun@gmail.com>
2020-12-19 19:11:37 +0900

    add mergedThreading.py

--- merged.py
+++ merged.py
@@ -10,27 +10,78 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 from pycocotools.coco import COCO
+import dlib
+from math import hypot
+import datetime
+import pygame
 
+############################# settings #############################
+# blink
+detector = dlib.get_frontal_face_detector()
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+LEFT_EYE_POINTS = list(range(36, 42))
+RIGHT_EYE_POINTS = list(range(42, 48))
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+BLINK_CYCLE_SEC = 5
+
+SOUND_LENTH = 2
+sound_time = SOUND_LENTH
+
+# pose
 pygame.mixer.init()
 bang = pygame.mixer.Sound("please_correct.wav")
 quitm = pygame.mixer.Sound("quit.wav")
+blink_sound = pygame.mixer.Sound("please_blink.wav") # blink sound
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
-
 
 APP_KEY = '61438e2034d5616b9ecaf5ab8ccf7bf7'
 
 session = requests.Session()
 session.headers.update({'Authorization': 'KakaoAK ' + APP_KEY})
- 
 
 ORIGIN_PATH = '/home/pi/HumanIron/origin.jpg'
 NEW_PATH = '/home/pi/HumanIron/new.jpg'
 
+############################# methods #############################
+# blink
+def midpoint(p1, p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(
+        eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(
+        eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(
+        eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(
+        eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    hor_line = cv2.line(image, left_point, right_point, (0, 255, 0), 2)
+    ver_line = cv2.line(image, center_top, center_bottom, (0, 255, 0), 2)
+
+    hor_line_lenght = hypot(
+        (left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_lenght = hypot(
+        (center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+    ratio = hor_line_lenght / ver_line_lenght
+    return ratio
+
+# pose
 def inference(filename):
     with open(filename, 'rb') as f:
         response = session.post('https://cv-api.kakaobrain.com/pose', files=[('file', f)])
         return response.json()
+
 
 def visualori(filename, annotations, threshold=0.2):
     for annotation in annotations:
@@ -64,27 +115,30 @@ def visualori(filename, annotations, threshold=0.2):
     coco.createIndex()
     coco.showAnns(annotations)
     plt.savefig('test.jpg')
-    
+
     img = cv2.imread('test.jpg', cv2.IMREAD_UNCHANGED)
     cv2.waitKey(1)
     return img
 
+
 def addpic(img, img2):
-    newimg = np.hstack((img, img2))   
+    newimg = np.hstack((img, img2))
     cv2.imshow('Result', newimg)
     cv2.waitKey(5000)
     cv2.destroyAllWindows()
 
+############################# main #############################
 
+last_time_blink = time.time()
 with picamera.PiCamera() as camera:
     camera.start_preview()
     frame = 1
     GPIO.wait_for_edge(17, GPIO.FALLING)
-    
+
     camera.capture(ORIGIN_PATH)
     orgimg = Image.open(ORIGIN_PATH)
-    orgimg.save(ORIGIN_PATH, quality=86)           
-    
+    orgimg.save(ORIGIN_PATH, quality=86)
+
     origin = inference(ORIGIN_PATH)
     img = visualori(ORIGIN_PATH, origin)
 
@@ -97,21 +151,21 @@ with picamera.PiCamera() as camera:
         img2 = visualori(NEW_PATH, newdata)
 
         try:
-            #캡쳐 이미자
+            # 캡쳐 이미자
             # 파일로 이미지 입력시
             oleft_shoulder = origin[0]["keypoints"][6]
             oright_shoulder = origin[0]["keypoints"][7]
-            onose =origin[0]["keypoints"][0]
-            #t분에 한번씩 이미지 저장
+            onose = origin[0]["keypoints"][0]
+            # t분에 한번씩 이미지 저장
             sleft_shoulder = newdata[0]["keypoints"][6]
             sright_shoulder = newdata[0]["keypoints"][7]
-            snose =newdata[0]["keypoints"][0]
-    
+            snose = newdata[0]["keypoints"][0]
+
             lsd = oleft_shoulder - sleft_shoulder
             rsd = oright_shoulder - sright_shoulder
             nd = onose - snose
 
-            if abs(lsd)>30 or abs(rsd)>30 or abs(nd)>30:
+            if abs(lsd) > 30 or abs(rsd) > 30 or abs(nd) > 30:
                 print(" alarm")
                 bang.play()
                 addpic(img, img2)
@@ -119,7 +173,7 @@ with picamera.PiCamera() as camera:
             else:
                 print(abs(lsd))
                 print(abs(rsd))
-                print(abs(nd))  
+                print(abs(nd))
                 camera.stop_preview()
                 time.sleep(
                     int(5) - (time.time() - start)
