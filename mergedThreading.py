9cb5aa7a94bf8e1740dda44a23f134e3d0d6d529
icymunchhhiip <fsec1sun@gmail.com>
2020-12-20 01:50:16 +0900

    edit mergedThreading.py

--- mergedThreading.py
+++ mergedThreading.py
@@ -139,7 +139,7 @@ async def blinkmain():
         global camera
         camera = picamera.PiCamera()
         camera.resolution = (640, 480)
-        camera.framerate = 30
+        camera.framerate = 1
         raw_capture = PiRGBArray(camera, size=(640, 480))
         time.sleep(0.1)
         for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
