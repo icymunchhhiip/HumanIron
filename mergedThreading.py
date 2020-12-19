1874129368689a1a52f30ccfd75f75ed4174f674
icymunchhhiip <fsec1sun@gmail.com>
2020-12-20 01:51:14 +0900

    edit mergedThreading.py

--- mergedThreading.py
+++ mergedThreading.py
@@ -135,13 +135,14 @@ async def blinkmain():
     sound_time = SOUND_LENTH
     last_time_blink = time.time()
 
+    global camera
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 1
+    raw_capture = PiRGBArray(camera, size=(640, 480))
+    time.sleep(0.1)
+
     while True:
-        global camera
-        camera = picamera.PiCamera()
-        camera.resolution = (640, 480)
-        camera.framerate = 1
-        raw_capture = PiRGBArray(camera, size=(640, 480))
-        time.sleep(0.1)
         for frame in camera.capture_continuous(raw_capture, format="bgr", use_video_port=True):
             # _, image = capture.read()
 
