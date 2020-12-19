4781cc822624071aaaaafa35b608fa9be8824348
icymunchhhiip <54310796+icymunchhhiip@users.noreply.github.com>
2020-12-19 17:14:38 +0900

    Update requireStretching.py

--- requireStretching.py
+++ requireStretching.py
@@ -4,10 +4,14 @@ import pygame
 pygame.mixer.init()
 stretching_sound = pygame.mixer.Sound("real_adult.wav")
 
-CYCLE_MIN = 3
-seconds = CYCLE_MIN * 60
+CYCLE_MIN = 20
+REST_TIME_MIN = 5
+
+cycle_seconds = CYCLE_MIN * 60
+rest_seconds = REST_TIME_MIN * 60
 
 while True:
-    time.sleep(seconds)
+    time.sleep(cycle_seconds)
     print("please stretching")
     stretching_sound.play()
+    time.sleep(rest_seconds)
